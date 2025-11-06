a8b8887bc80f4733ebe2e701e095eb5678aeb433
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-11-05 23:24:48 -0500

    Feat(WebClient): Creating the handler component

---
+++ src/main/java/com/bolsadeideas/springboot/webflux/client/app/handler/ProductoHandler.java
@@ -0,0 +1,36 @@
+package com.bolsadeideas.springboot.webflux.client.app.handler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.bolsadeideas.springboot.webflux.client.app.models.Producto;
+import com.bolsadeideas.springboot.webflux.client.app.models.services.ProductoService;
+
+import static org.springframework.web.reactive.function.BodyInserters.*;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class ProductoHandler {
+
+    @Autowired
+    private ProductoService service;
+
+    public Mono<ServerResponse> listar(ServerRequest request) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.findAll(), Producto.class);
+    }
+
+    public Mono<ServerResponse> ver(ServerRequest request) {
+        String id = request.pathVariable("id");
+        return service.findById(id)
+                .flatMap(producto -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(producto)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+}
