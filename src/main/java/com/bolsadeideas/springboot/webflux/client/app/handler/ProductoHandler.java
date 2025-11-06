95ec68b81871f85b7a254676214de423e3ca1aaf
kevinalexisesma <81497048+kevinalexisesma@users.noreply.github.com>
2025-11-06 16:32:40 +0000

    Feat(WebClient): Implementing CRUD in the Handler

--- src/main/java/com/bolsadeideas/springboot/webflux/client/app/handler/ProductoHandler.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/client/app/handler/ProductoHandler.java
@@ -2,6 +2,7 @@ package com.bolsadeideas.springboot.webflux.client.app.handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -10,6 +11,9 @@ import com.bolsadeideas.springboot.webflux.client.app.models.Producto;
 import com.bolsadeideas.springboot.webflux.client.app.models.services.ProductoService;
 
 import static org.springframework.web.reactive.function.BodyInserters.*;
+
+import java.net.URI;
+import java.util.Date;
 
 import reactor.core.publisher.Mono;
 
@@ -32,5 +36,33 @@ public class ProductoHandler {
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(fromValue(producto)))
                 .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> crear(ServerRequest request) {
+        Mono<Producto> producto = request.bodyToMono(Producto.class);
+        return producto
+                .flatMap(p -> {
+                    if (p.getCreateAt() == null) {
+                        p.setCreateAt(new Date());
+                    }
+                    return service.save(p);
+                })
+                .flatMap(p -> ServerResponse.created(URI.create("api/client/".concat(p.getId())))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(fromValue(p)));
+    }
+
+    public Mono<ServerResponse> editar(ServerRequest request) {
+        String id = request.pathVariable("id");
+        Mono<Producto> producto = request.bodyToMono(Producto.class);
+        return producto
+                .flatMap(p -> ServerResponse.created(URI.create("/api/client/".concat(id)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(service.update(p, id), Producto.class));
+    }
+
+    public Mono<ServerResponse> eliminar(ServerRequest request) {
+        String id = request.pathVariable("id");
+        return service.delete(id).then(ServerResponse.noContent().build());
     }
 }
